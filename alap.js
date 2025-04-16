481a9f66778fe57346e6ad0a765c5c96c2ffb5ba
Aniko <nahajia@gmail.com>
2025-04-16 11:37:04 +0200

    modális kezdet

--- alap.js
+++ alap.js
@@ -5,19 +5,24 @@ fetch("https://api.nobelprize.org/v1/prize.json")
 function megjelenit(y){
     console.log(y)
     let sz=""
+    let sorszam=0
     for (const elem of y.prizes) {
         sz+=`
         <div class="col-sm-3">
-            <div class="szegely">
+            <div class="szegely" data-bs-toggle="modal" data-bs-target="#myModal" onclick="reszletKiir(${sorszam})" >
                 <p>${elem.year}</p>
                 <p>${elem.category}</p>
             </div>
         </div>
+
         `
+        sorszam++
     }
     document.getElementById("keret").innerHTML=sz
 
 }
 
-
+function reszletKiir(sorszam){
+    console.log(sorszam)
+}
 
