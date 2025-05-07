7491e6b46863159c15b5b71566fe10be728e42d2
Aniko <nahajia@gmail.com>
2025-05-07 11:41:27 +0200

    nev keres

--- nevkeres.js
+++ nevkeres.js
@@ -6,13 +6,48 @@ function keresesNev(){
 }
 
 function nevMegjelenit(y,beNev){
+    let sz=`
+     <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Év,Kategória</th>
+        <th>Keresztnév</th>
+        <th>Vezetéknév</th>
+        <th>Indok</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+   
+  `
     for (const elem of y.prizes) {
         if (elem.laureates!=undefined)
             for (const alElem of elem.laureates) {
                 
-                if (alElem.firstname==beNev){
+                if (alElem.firstname.toLowerCase().includes(beNev.toLowerCase()) || alElem.motivation.toLowerCase().includes(beNev.toLowerCase())  ){
                     console.log(alElem.firstname)
+                    sz+=`
+                    <tr>
+                    <td>${elem.year} ${elem.category}</td>
+                    <td>${alElem.firstname}</td>
+                    <td>${alElem.surname}</td>
+                    <td>${alElem.motivation}</td>
+                    </tr>
+                    `
                 }
+                // -------------------surname vizsgálata külön---------------------
+                if (alElem.surname!=undefined && alElem.surname.toLowerCase().includes(beNev.toLowerCase())  ){
+                    console.log(alElem.firstname)
+                    sz+=`
+                    <tr>
+                    <td>${elem.year} ${elem.category}</td>
+                    <td>${alElem.firstname}</td>
+                    <td>${alElem.surname}</td>
+                    <td>${alElem.motivation}</td>
+                    </tr>
+                    `
+                }
+
         }
         else 
             console.log(elem.year+" "+elem.category)
@@ -20,6 +55,9 @@ function nevMegjelenit(y,beNev){
     }
 
 
+    sz+=` </tbody>
+  </table>`
+  document.getElementById("tablazat").innerHTML=sz
 }
 
 
